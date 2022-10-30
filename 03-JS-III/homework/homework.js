c0001685471237951fd47270b5ef907c47c5c763
zuttatenso17 <zutta1000@yahoo.com.ar>
2022-10-30 14:49:12 -0500

    ejercicio

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,30 +8,53 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var acumulador = 0
+    for (var i = 1; i < 11; i++) {
+      acumulador = acumulador + i 
+     }
+     return acumulador 
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+var pares = []
+for (var i = 0; i < array.length; i++) {
+  if (array [i] % 2 === 0) {
+    pares.push (array [i])
+    }
+  }
+return pares
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
-}
+var resultado = array.map(function (valor) {
+  return valor ** 2
+})
+return resultado
+}  
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var acumulador = 0
+  for (var i = 0; i < array.length; i++) {
+    acumulador = acumulador + array [i];
+  }
+  return acumulador
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  return num.toString().length
 }
+
   
   
   // No modificar nada debajo de esta línea
